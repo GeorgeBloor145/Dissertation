bcc2ff80c873867cdc59ee657fefe94cac3965c7
George <joe@riptidewave.com>
2022-02-16 03:33:27 +0000

    Added PCAP and added pcap_processing function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 ### WEEEEEE
 
+from scapy.all import *
+
+
+def pcap_processing():
+
+    pcap = rdpcap('fuzz-2006-06-26-2594.pcap')
+    ips = set((p[IP].src) for p in PcapReader('fuzz-2006-06-26-2594.pcap') if IP in p)
+    for i in ips:
+        print(i)
+
+pcap_processing()
 
