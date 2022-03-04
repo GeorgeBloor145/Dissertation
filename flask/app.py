5be4c4e4b96c8ad460b5a4b3a821163df23fedf9
George Bloor <b8006033@my.shu.ac.uk>
2022-03-04 07:44:54 -0800

    Updated flask files

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
-
+name = "George"
 @app.route("/")
 def home():
-    return "Hello, main page"
+    return render_template("index.html", content=name) #in html file, use {{}} with var inside to pass variables. backend use front end var name with back end var
 
 
 
