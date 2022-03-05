b41f4a8658fa503eb1a10f715f611113142220a4
George Bloor <b8006033@my.shu.ac.uk>
2022-03-05 07:51:25 -0800

    Html files and nav bar

--- flask/app.py
+++ flask/app.py
@@ -3,15 +3,16 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 name = "George"
 @app.route("/")
+def welcome():
+    return render_template("welcome.html", content=name) #in html file, use {{}} with var inside to pass variables. backend use front end var name with back end var
+
+@app.route("/home")
 def home():
-    return render_template("index.html", content=name) #in html file, use {{}} with var inside to pass variables. backend use front end var name with back end var
+    return  render_template("home.html")
 
 
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug =True)
 
